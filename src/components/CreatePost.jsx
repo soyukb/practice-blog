a3003908f30e9799070e805861fb83da7791d1eb
soyukb <soyukoba@gmail.com>
2023-11-03 17:10:35 +0900

    コメントを入れる

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./CreatePost.css"
+import { collection, addDoc } from "firebase/firestore"; 
+import { db, auth } from "../firebase";
+import { useNavigate } from 'react-router-dom';
 
 const CreatePost = () => {
+  const [title, setTitle] = useState()
+  const [postText, setPostText] = useState()
+  const navigate = useNavigate()
+
+  const createPost = async() => {
+    
+    // Add a new document with a generated id.
+    const docRef = await addDoc(collection(db, "posts"), {
+      title: "title",
+      postText: "postText",
+      author: { 
+        username : auth.currentUser.displayName,
+        id : auth.currentUser.uid
+      }
+
+    });
+    navigate("/")
+  }
+
   return (
+
+
     <div className='createPostPage'>
       <div className="postContainer">
         <h1>記事を投稿する</h1>
         <div>タイトル</div>
-        <input type="text" placeholder='タイトルを記入' />
+        <input type="text" placeholder='タイトルを記入' 
+          onChange={(e)=>setTitle(e.target.value)}
+           />
         <div>投稿</div>
-        <textarea placeholder='投稿内容を記入' />        
-        <button className="postButton">投稿する</button>
+        <textarea placeholder='投稿内容を記入' 
+          onChange={(e)=>setPostText(e.target.value)}/>        
+        <button className="postButton" onClick={createPost}>投稿する</button>
       </div>
     </div>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import "./Home.css"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="homePage">
+      <div className="postContent">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+      
+      <div className="postTextConteiner">今はReact学習中です。</div>
+      <div className="nameAndDeleteButton">
+        <h3>@soyukoba</h3>
+        <button>削除</button>
+      </div>
+      </div>
+    </div>
   )
 }
 
